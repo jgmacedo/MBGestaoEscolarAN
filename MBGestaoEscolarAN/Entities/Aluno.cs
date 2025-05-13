babc32b033a22ed9f6fba28fac06127e6477599d
joao.gabrielm <joao.gabrielm@AN0101704085941>
2025-05-12 21:56:53 -0300

    Adicionar arquivos de projeto.

---
+++ MBGestaoEscolarAN/Entities/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace MBGestaoEscolarAN.Entities
+{
+    public class Aluno
+    {
+        private int alunoId;
+        private string nome;
+        private string CPF;
+        private string matricula;
+        private string email;
+        private string telefone;
+        private DateTime dataNascimento;
+        private string endereco;
+        private string statusAluno;
+        private DateTime dataCadastro;
+
+        public int AlunoId { get => alunoId; set => alunoId = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string CPF1 { get => CPF; set => CPF = value; }
+        public string Matricula { get => matricula; set => matricula = value; }
+        public string Email { get => email; set => email = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+        public DateTime DataNascimento { get => dataNascimento; set => dataNascimento = value; }
+        public string Endereco { get => endereco; set => endereco = value; }
+        public string StatusAluno { get => statusAluno; set => statusAluno = value; }
+        public DateTime DataCadastro { get => dataCadastro; set => dataCadastro = value; }
+    }
+}
---
+++ MBGestaoEscolarAN/Entities/Curso.cs
@@ -0,0 +1,27 @@
+﻿namespace MBGestaoEscolarAN.Entities
+{
+    public class Curso
+    {
+        private int cursoId;
+        private string nome;
+        private string codigo;
+        private string descricao;
+        private int cargaHoraria;
+        private string modalidade;
+        private string trilha;
+        private int pontosCAP;
+        private string statusCurso;
+        private int coordenadorID;
+
+        public int CursoId { get => cursoId; set => cursoId = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+        public int CargaHoraria { get => cargaHoraria; set => cargaHoraria = value; }
+        public string Modalidade { get => modalidade; set => modalidade = value; }
+        public string Trilha { get => trilha; set => trilha = value; }
+        public int PontosCAP { get => pontosCAP; set => pontosCAP = value; }
+        public string StatusCurso { get => statusCurso; set => statusCurso = value; }
+        public int CoordenadorID { get => coordenadorID; set => coordenadorID = value; }
+    }
+}
